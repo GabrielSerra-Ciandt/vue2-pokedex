5970085d75b5914ad6d9301523ea5365fcb2944e
GabrielSerra_Ci&t <gabrielserra@ciandt.com>
2022-06-09 09:28:50 -0300

    inicio dev quarkus

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vue2-pokedex
 Repo para o desafio Pokedes - Onbording Stags
+
+# Create Quarkus project
+https://code.quarkus.io/
+
+# Start
+$ mvn quarkus:dev
---
+++ quarkus-pokemon-api/src/main/java/co/pokeapi/resources/restclient/POKEAPIRestClient.java
@@ -0,0 +1,7 @@
+package co.pokeapi.resources.restclient;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient()
+public interface POKEAPIRestClient {
+}
