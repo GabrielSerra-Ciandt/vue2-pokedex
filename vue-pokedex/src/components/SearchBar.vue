e7f6765a12add02e48ce59362a6182014614fd2a
GabrielSerra_Ci&t <gabrielserra@ciandt.com>
2022-07-01 13:03:52 -0300

    [FRONT] Criação da pagina principal da Pokedex

---
+++ vue-pokedex/src/components/SearchBar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>SearchBar</h1>
+    </div>
+</template>
--- vue-pokedex/src/views/PokeView.vue
+++ vue-pokedex/src/views/PokeView.vue
@@ -1,5 +1,29 @@
 <template>
-  <h1>Poke</h1>
+  <div class="logo">
+    <img alt="Pokemon Logo" src="../assets/PokemonLogo.png" />
+    <SearchBar />
+    <Pokedex />
+  </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import Pokedex from "@/components/Pokedex.vue";
+import SearchBar from '@/components/SearchBar.vue';
 
+export default {
+  name: "PokeView",
+  components: {
+    Pokedex,   
+    SearchBar
+  },
+};
+</script>
+
+
+<style scoped>
+img {
+  max-width: 30%;
+  max-height: 30%;
+}
+</style>
