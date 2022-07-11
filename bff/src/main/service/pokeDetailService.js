3178ecfb027ca852c6b7cbe7c5d697bc9e58e146
GabrielSerra_Ci&t <gabrielserra@ciandt.com>
2022-07-11 12:08:36 -0300

    Correcao do Range Pokemons

--- bff/src/main/service/pokeDetailService.js
+++ bff/src/main/service/pokeDetailService.js
@@ -1,25 +1,27 @@
 const apiRepository = require('../gateways/http/pokeApiRepository.js');
 
-const maxPokemons = 898;
+const maxRangePokemons = 898;
 const pokemonDetail = {};
 
 const pokeDetailService = {
-    getPokemonDatail: async (req, res) =>{  
-        
-        if (req <= maxPokemons){       
-        let results = await apiRepository.getPokeDetail(req, res);
-        
-        createPokemonDetail(results);
-        console.log(pokemonDetail);//////////////////////////////////////Tirar console.log
-        res.send(pokemonDetail);
-        }
+    getPokemonDatail: async (req, res) => {        
 
-        res.status(500).send(`Pokemon ID out off range (Range=${maxPokemons})`)
+        if (req.params.pokeId > maxRangePokemons || req.params.pokeId <= 0) {
+            res.status(500).send(`Pokemon ID out off range (Range=${maxRangePokemons})`)
+
+        } else {
+
+            let results = await apiRepository.getPokeDetail(req, res);
+
+            createPokemonDetail(results);
+            console.log(pokemonDetail);//////////////////////////////////////Tirar console.log
+            res.send(pokemonDetail);
+        }
     }
 
 }
 
-function createPokemonDetail(results){ 
+function createPokemonDetail(results) {
 
     pokemonDetail.id = results.id;
     pokemonDetail.name = results.forms[0].name;
@@ -28,10 +30,10 @@ function createPokemonDetail(results){
     pokemonDetail.height = results.height;
     pokemonDetail.weight = results.weight;
     pokemonDetail.type1 = results.types[0].type.name;
-    pokemonDetail.type2 = results.types[1]?.type.name;   
+    pokemonDetail.type2 = results.types[1]?.type.name;
     pokemonDetail.ability1 = results.abilities[0].ability.name;
-    pokemonDetail.ability2 = results.abilities[1]?.ability.name;        
-    
+    pokemonDetail.ability2 = results.abilities[1]?.ability.name;
+
 }
 
 module.exports = pokeDetailService;
